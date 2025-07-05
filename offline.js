d75307fbbaa99015475f31190252ceb92de75273
jazzcript <jazzcript@hotmail.com>
2025-07-06 00:28:35 +0100

    PRESENTABLE! Trees and circle pack added

--- offline.js
+++ offline.js
@@ -1,12 +1,18 @@
 self.addEventListener('install', e =>
   e.waitUntil(
-    caches.open('offline').then(c => c.addAll(['/', '/favicon.ico']))
+    caches.open('offline').then(c => c.addAll([
+      '/', '/favicon.ico', '/circle-pack.html', '/tidy-tree.html', '/cluster-tree.html'
+    ]))
   )
 );
 
 self.addEventListener('fetch', e => e.respondWith(
   fetch(e.request).then(r => {
-    caches.open('offline').then(c => c.put(e.request, r.clone()));
+    caches.open('offline').then(c => {
+      try {
+        return c.put(e.request, r.clone())
+      } catch(err){}
+    });
     return r;
   }).catch(() =>
     caches.match(e.request).then(r => r || Response.error())
